b62a5db21c1fcb9a36c6f6f8ffbed6719f16a34a
Febriani F D <febrianifdamanik@gmail.com>
2017-08-03 15:00:22 +0700

    Add files via upload

---
+++ Array 02 - Nama Bulan (Console)/Array 02 - Nama Bulan (Console)/Module1.vb
@@ -0,0 +1,21 @@
+﻿'Catatan:
+'Dim asBulan() As String = {"Januari", "Februari", dst.}
+'boleh ditulis:
+'Dim asBulan() = {"Januari", "Februari", dst.}
+
+Module Module1
+
+    Sub Main()
+        Dim asBulan() As String = {"Januari", "Februari", "Maret", "April", "Mei", _
+                                   "Juni", "Juli", "Agustus", "September", "Oktober", _
+                                   "November", "Desember"}
+        Dim I As Byte
+
+        Console.WriteLine("Nama-Nama Bulan:")
+        For I = 0 To UBound(asBulan)
+            Console.WriteLine(I + 1 & ": " & asBulan(I))
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
