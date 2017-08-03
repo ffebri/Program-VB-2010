f251b88b567efaa01ee285d8e8923994558b7ef4
Febriani F D <febrianifdamanik@gmail.com>
2017-08-03 15:29:34 +0700

    Add files via upload

---
+++ Array 05 - Array Dinamis (Console)/Array 05 - Array Dinamis (Console)/Module1.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim I As Integer
+        Dim aiA() As Integer = {1, 2, 3}
+        Dim aiB() As Integer = {1, 2, 3}
+
+        Console.WriteLine("Array Dinamis." & vbCrLf)
+
+        Console.WriteLine("Array aiA sebelum ReDim:")
+        For I = 0 To UBound(aiA)
+            Console.Write(aiA(I) & vbTab)
+        Next
+        Console.WriteLine("")
+
+        ReDim aiA(4) 'Semua nilai array diinisialisasikan ke nilai default (untuk numerik = 0)
+
+        Console.WriteLine("Array aiA setelah ReDim:")
+        For I = 0 To UBound(aiA)
+            Console.Write(aiA(I) & vbTab)
+        Next
+        Console.WriteLine("")
+
+        Console.WriteLine("Array aiB sebelum ReDim Preserve:")
+        For I = 0 To UBound(aiB)
+            Console.Write(aiB(I) & vbTab)
+        Next
+        Console.WriteLine("")
+
+        ReDim Preserve aiB(4) 'Nilai lama dipertahankan
+
+        Console.WriteLine("Array aiB setelah ReDim Preserve:")
+        For I = 0 To UBound(aiB)
+            Console.Write(aiB(I) & vbTab)
+        Next
+        Console.WriteLine("")
+        Console.ReadKey()
+    End Sub
+
+End Module
