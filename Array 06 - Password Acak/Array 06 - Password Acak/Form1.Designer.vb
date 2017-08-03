8c50ee593427c076d5a7092a1478e85b0db60309
Febriani F D <febrianifdamanik@gmail.com>
2017-08-03 15:32:20 +0700

    Add files via upload

---
+++ Array 06 - Password Acak/Array 06 - Password Acak/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnBuat = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtJumKar = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(17, 41)
+        Me.txtPassword.Multiline = True
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.ReadOnly = True
+        Me.txtPassword.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtPassword.Size = New System.Drawing.Size(171, 205)
+        Me.txtPassword.TabIndex = 2
+        '
+        'btnBuat
+        '
+        Me.btnBuat.Location = New System.Drawing.Point(204, 38)
+        Me.btnBuat.Name = "btnBuat"
+        Me.btnBuat.Size = New System.Drawing.Size(86, 25)
+        Me.btnBuat.TabIndex = 3
+        Me.btnBuat.Text = "&Buat Password"
+        Me.btnBuat.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Jumlah karakter (min. 3)"
+        '
+        'txtJumKar
+        '
+        Me.txtJumKar.Location = New System.Drawing.Point(145, 12)
+        Me.txtJumKar.Name = "txtJumKar"
+        Me.txtJumKar.Size = New System.Drawing.Size(36, 20)
+        Me.txtJumKar.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(316, 262)
+        Me.Controls.Add(Me.txtJumKar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnBuat)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Name = "Form1"
+        Me.Text = "Password Acak"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnBuat As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtJumKar As System.Windows.Forms.TextBox
+
+End Class
---
+++ Array 06 - Password Acak/Array 06 - Password Acak/Form1.vb
@@ -0,0 +1,72 @@
+﻿'Program untuk membuat password acak minimal 3 karakter yang terdiri
+'dari huruf dan angka dengan ketentuan:
+'1. Minimal harus ada 1 huruf kapital, 1 huruf kecil, dan 1 angka.
+'2. Karakter pertama harus huruf, boleh huruf kapital atau huruf kecil.
+
+Public Class Form1
+
+    Private Sub btnBuat_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBuat.Click
+        Const BATAS = 25
+        Dim asPassword(BATAS - 1) As String, sPassword As String
+        Dim I, J, JumKar, Acak As Integer
+        Dim blnKapital, blnKecil, blnAngka As Boolean, blnPswdSudahAda As Boolean
+
+        JumKar = Val(txtJumKar.Text)
+        If JumKar < 3 Then
+            MsgBox("Jumlah karakter password minimal harus 3!", , "Pesan Kesalahan")
+            Exit Sub
+        End If
+
+        Randomize() 'Inisialisasi pembangkit bilangan random
+        I = 0
+        Do
+            blnKapital = False : blnKecil = False : blnAngka = False
+            sPassword = ""
+            For J = 1 To JumKar
+                If J = 1 Then 'karakter pertama harus huruf
+                    Acak = Int(Rnd() * 2) 'Angka acak 0, 1
+                    'karakter ketujuh diisi dengan huruf kapital jika huruf kapital belum ada
+                ElseIf J = JumKar - 1 And blnKapital = False Then
+                    Acak = 0
+                    'karakter ketujuh diisi dengan huruf kecil jika huruf kecil belum ada
+                ElseIf J = JumKar - 1 And blnKecil = False Then
+                    Acak = 1
+                    'karakter ketujuh diisi dengan angka jika angka belum ada
+                ElseIf J = JumKar And blnAngka = False Then
+                    Acak = 2
+                Else
+                    Acak = Int(Rnd() * 3) 'Angka acak 0, 1, 2
+                End If
+                If Acak = 0 Then 'huruf kapital
+                    blnKapital = True
+                    Acak = Int(Rnd() * 26) + 65 'Angka acak (0, 1, 2,..., 25) + 65
+                ElseIf Acak = 1 Then 'huruf kecil
+                    blnKecil = True
+                    Acak = Int(Rnd() * 26) + 97 'Angka acak (0, 1, 2,..., 25) + 97
+                Else 'angka
+                    blnAngka = True
+                    Acak = Int(Rnd() * 10) + 48 'Angka acak (0, 1, 2,..., 9) + 48
+                End If
+                sPassword = sPassword & Chr(Acak)
+            Next
+
+            'Periksa apakah password ini sudah ada.
+            blnPswdSudahAda = False
+            For J = 0 To I - 1
+                If asPassword(J) = sPassword Then
+                    blnPswdSudahAda = True
+                    Exit For
+                End If
+            Next
+            If Not blnPswdSudahAda Then
+                asPassword(I) = sPassword
+                I += 1
+            End If
+        Loop Until I = BATAS
+
+        txtPassword.Text = ""
+        For I = 0 To BATAS - 1
+            txtPassword.Text = txtPassword.Text & asPassword(I) & vbCrLf
+        Next
+    End Sub
+End Class
